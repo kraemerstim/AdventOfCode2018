92a84b1043ea3ce41ca03fb410dc88225489f814
Tim Kraemer <kraemerstim@gmail.com>
2018-12-10 08:36:17 +0100

    tag 9

--- 9.py
+++ 9.py
@@ -1,5 +1,5 @@
-players = 10
-last_marble_worth = 1618
+players = 411
+last_marble_worth = 72059
 
 current_marble_index = 1
 marble_circle = [0, 4, 2, 1, 3]
@@ -13,7 +13,13 @@ def take_turn():
     global next_available_marble
     global current_player
     if next_available_marble % 23 == 0:
-        player_score[current_player] += 23
+        additional_score = next_available_marble
+        remove_marble_index = (current_marble_index-7)%len(marble_circle)
+        additional_score += marble_circle[remove_marble_index]
+        marble_circle = marble_circle[:remove_marble_index] + marble_circle[remove_marble_index+1:]
+        current_marble_index = (remove_marble_index)%len(marble_circle)
+        player_score[current_player] += additional_score
+        #print(str(next_available_marble) + ': ' + str(additional_score) + ' ... ' + str(player_score))
     else:
         new_index = (current_marble_index+2)%len(marble_circle)
         marble_circle = marble_circle[:new_index] +[next_available_marble] + marble_circle[new_index:]
@@ -21,7 +27,13 @@ def take_turn():
     next_available_marble += 1
     current_player = (current_player+1)%players
 
-for i in range(28):
+while True:
     take_turn()
-    print(str(marble_circle) + ' ' + str(current_marble_index))
+    if next_available_marble > last_marble_worth:
+        print(max(player_score))
+        break
+
+#for i in range(2000):
+#    take_turn()
+#    print(str(marble_circle) + ' ' + str(current_marble_index))
 
